ea7095bec332d94c4ad271805dad97c4b3d3b4d1
Vlad Petyuk <petyuk@gmail.com>
2014-07-11 11:03:22 -0700

    band-aid for letter case handling

--- R/read_mzIDs.R
+++ R/read_mzIDs.R
@@ -10,7 +10,64 @@
       data <- data.table(flatten(mzID(mzids)))
       saveCache(data, key=key)
    }
+   
+   # the piece below will be removed in the future,
+   # one the corresponding functionality implemented in mzID
+   # do some cleaning/tweaking of column modes and name casing
+   
+   # Columns that must be present:
+   # Peptide, Accession, isDecoy, calculatedMassToCharge, 
+   # experimentalMassToCharge, chargeState, spectrumFile, spectrumID
+   if(!is.null(data$accession)){
+      data$Accession <- data$accession
+      data$accession <- NULL
+   }
+
+   if(!is.null(data$isdecoy)){
+      data$isDecoy <- data$isdecoy
+      data$isdecoy <- NULL
+   }
+   
+   if(!is.null(data$calculatedmasstocharge)){
+      data$calculatedMassToCharge <- as.numeric(data$calculatedmasstocharge)
+      data$calculatedmasstocharge <- NULL
+   }
+   
+   if(!is.null(data$experimentalmasstocharge)){
+      data$experimentalMassToCharge <- as.numeric(data$experimentalmasstocharge)
+      data$experimentalmasstocharge <- NULL
+   }
+   
+   if(!is.null(data$chargestate)){
+      data$chargeState <- data$chargestate
+      data$chargestate <- NULL
+   }
+   
+   if(!is.null(data$spectrumid)){
+      data$spectrumID <- data$spectrumid
+      data$spectrumid <- NULL
+   }
+   
+   if(!is.null(data$pre)){
+      data$Pre <- data$pre
+      data$pre <- NULL
+   }
+   
+   if(!is.null(data$post)){
+      data$Post <- data$post
+      data$post <- NULL
+   }
+   
+   if(!is.null(data$pepseq)){
+      data$PepSeq <- data$pepseq
+      data$pepseq <- NULL
+   }
+   #---
+   data$Peptide <- paste(data$Pre, data$PepSeq, data$Post, sep='.')
+   
    return(data)
 }
+
+
 
 
