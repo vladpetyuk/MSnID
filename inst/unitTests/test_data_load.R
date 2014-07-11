5bd0299631672488398d907597232ba37f80cc12
Vlad Petyuk <petyuk@gmail.com>
2014-07-11 11:04:18 -0700

    adjusting to name handling in read_mzIDs

--- inst/unitTests/test_data_load.R
+++ inst/unitTests/test_data_load.R
@@ -2,22 +2,22 @@ library("MSnID")
 msnid2 <- MSnID(".")
 mzids <- system.file("extdata","c_elegans.mzid.gz",package="MSnID")
 msnid2 <- read_mzIDs(msnid2, mzids)
-msnid2$Accession <- msnid2$accession
-msnid2$accession <- NULL
-msnid2$isDecoy <- msnid2$isdecoy
-msnid2$isdecoy <- NULL
-msnid2$calculatedMassToCharge <- msnid2$calculatedmasstocharge
-msnid2$calculatedmasstocharge <- NULL
-msnid2$experimentalMassToCharge <- msnid2$experimentalmasstocharge
-msnid2$experimentalmasstocharge <- NULL
-msnid2$chargeState <- msnid2$chargestate
-msnid2$chargestate <- NULL
-msnid2$spectrumID <- msnid2$spectrumid
-msnid2$spectrumid <- NULL
-msnid2$Peptide <- paste(msnid2$pre, msnid2$pepseq, msnid2$post, sep='.')
-msnid2$pre <- NULL
-msnid2$post <- NULL
-msnid2$pepseq <- NULL
+# msnid2$Accession <- msnid2$accession
+# msnid2$accession <- NULL
+# msnid2$isDecoy <- msnid2$isdecoy
+# msnid2$isdecoy <- NULL
+# msnid2$calculatedMassToCharge <- msnid2$calculatedmasstocharge
+# msnid2$calculatedmasstocharge <- NULL
+# msnid2$experimentalMassToCharge <- msnid2$experimentalmasstocharge
+# msnid2$experimentalmasstocharge <- NULL
+# msnid2$chargeState <- msnid2$chargestate
+# msnid2$chargestate <- NULL
+# msnid2$spectrumID <- msnid2$spectrumid
+# msnid2$spectrumid <- NULL
+# msnid2$Peptide <- paste(msnid2$Pre, msnid2$PepSeq, msnid2$Post, sep='.')
+# msnid2$pre <- NULL
+# msnid2$post <- NULL
+# msnid2$pepseq <- NULL
 # clean up the cache directory
 unlink(".Rcache", recursive=TRUE)
 
