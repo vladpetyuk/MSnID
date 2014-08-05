64d832955ec59d0b2810879b95b42cea430b9884
Vlad Petyuk <petyuk@gmail.com>
2014-08-04 17:21:13 -0700

    changing tabs from 3 to 4 spaces

--- demo/c_elegans.R
+++ demo/c_elegans.R
@@ -51,8 +51,8 @@ pepCleav <- unique(psms(msnid)[,c("NumMissCleavages", "isDecoy", "Peptide")])
 pepCleav <- as.data.frame(table(pepCleav[,c("NumMissCleavages", "isDecoy")]))
 library("ggplot2")
 ggplot(pepCleav, aes(x=NumMissCleavages, y=Freq, fill=isDecoy)) + 
-   geom_bar(stat="identity", position="dodge") + 
-   ggtitle("Number of Missed Cleavages")
+    geom_bar(stat="identity", position="dodge") + 
+    ggtitle("Number of Missed Cleavages")
 # number of cysteins per peptide sequence as an example of sequence analysis
 msnid$NumCys <- sapply(lapply(strsplit(msnid$Peptide,''),'==','C'),sum)
 # calculating peptide lengths
@@ -60,8 +60,8 @@ msnid$PepLength <- nchar(msnid$Peptide) - 4
 pepLen <- unique(psms(msnid)[,c("PepLength", "isDecoy", "Peptide")])
 # distribution of peptide lengths in the dataset
 ggplot(pepLen, aes(x=PepLength, fill=isDecoy)) + 
-   geom_histogram(position='dodge', binwidth=3) +
-   ggtitle("Distribution on of Peptide Lengths")
+    geom_histogram(position='dodge', binwidth=3) +
+    ggtitle("Distribution on of Peptide Lengths")
 #------------------------------------------
 
 
@@ -82,52 +82,53 @@ show(msnid)
 # original mass measurement error in ppm
 ppm <- mass_measurement_error(msnid)
 ggplot(as.data.frame(ppm), aes(x=ppm)) + 
-   geom_histogram(binwidth=100)
+    geom_histogram(binwidth=100)
 # In this particular case the problem that it goes
 # far is that there was no certainity that the reported
 # masses are the masses of monoisotopic peaks.
 # Let's take a look at mass measurement error in Dalton units.
-dM <- with(psms(msnid), (experimentalMassToCharge-calculatedMassToCharge)*chargeState)
+dM <- with(psms(msnid), 
+            (experimentalMassToCharge-calculatedMassToCharge)*chargeState)
 x <- data.frame(dM, isDecoy=msnid$isDecoy)
 ggplot(x, aes(x=dM, fill=isDecoy)) + 
-   geom_histogram(position='stack', binwidth=0.1)
+    geom_histogram(position='stack', binwidth=0.1)
 # Fixing the peak picking problem
 msnid.fixed <- correct_peak_selection(msnid)
 # Now the errors are confined within 20 ppm
 ppm <- mass_measurement_error(msnid.fixed)
 ggplot(as.data.frame(ppm), aes(x=ppm)) + 
-   geom_histogram(binwidth=0.25)
+    geom_histogram(binwidth=0.25)
 # alternatively we can ignore erroneously picked picks
 # simply filter the data +/- 20 ppm
 msnid.chopped <- apply_filter(msnid, "abs(mass_measurement_error(msnid)) < 20")
 ppm <- mass_measurement_error(msnid.chopped)
 ggplot(as.data.frame(ppm), aes(x=ppm)) + 
-   geom_histogram(binwidth=0.25)
+    geom_histogram(binwidth=0.25)
 # The data can be recalibrated to remove any 
 # systematic components in the mass measurement error.
 msnid <- recalibrate(msnid.chopped)
 ppm <- mass_measurement_error(msnid)
 ggplot(as.data.frame(ppm), aes(x=ppm)) + 
-   geom_histogram(binwidth=0.25)
+    geom_histogram(binwidth=0.25)
 #------------------------------------------------
 
 
 # ---- MS/MS FILTER ------------------------
 # First filtering criteria - MS-GF Spec E-value
-msnid$msmsScore <- -log10(msnid$`ms-gf:specevalue`)
+msnid$msmsScore <- -log10(msnid$`MS-GF:SpecEValue`)
 # Second filtering criteria - absolute mass measurement error
 msnid$absParentMassErrorPPM <- abs(mass_measurement_error(msnid))
 # visualization
 params <- psms(msnid)[,c("msmsScore","absParentMassErrorPPM","isDecoy")]
 ggplot(params) + 
-   geom_density(aes(x = msmsScore, color = isDecoy, ..count..))
+    geom_density(aes(x = msmsScore, color = isDecoy, ..count..))
 ggplot(params) + 
-   geom_density(aes(x = absParentMassErrorPPM, color = isDecoy, ..count..))
+    geom_density(aes(x = absParentMassErrorPPM, color = isDecoy, ..count..))
 # subsetting params to top 10000 to accelerate the plotting
 set.seed(0)
 params <- params[sample.int(nrow(msnid),10000),]
 ggplot(data=params, aes(x=msmsScore, y=absParentMassErrorPPM, color=isDecoy)) +
-   geom_point(size=1.5)
+    geom_point(size=1.5)
 # Setting up filter object
 filtObj <- MSnIDFilter(msnid)
 filtObj$absParentMassErrorPPM <- list(comparison="<", threshold=10.0)
@@ -135,37 +136,39 @@ filtObj$msmsScore <- list(comparison=">", threshold=10.0)
 # print and visualize filter
 show(filtObj)
 ggplot(data=params, aes(x=msmsScore, y=absParentMassErrorPPM, color=isDecoy)) +
-   geom_point(size=1.5) + 
-   geom_hline(yintercept=filtObj$absParentMassErrorPPM$threshold, 
-              linetype='dashed') + 
-   geom_vline(xintercept=filtObj$msmsScore$threshold, 
-              linetype='dashed')
+    geom_point(size=1.5) + 
+    geom_hline(yintercept=filtObj$absParentMassErrorPPM$threshold, 
+        linetype='dashed') + 
+    geom_vline(xintercept=filtObj$msmsScore$threshold, 
+        linetype='dashed')
 # Evaluate filter. Check how well it performs at different levels.
 evaluate_filter(msnid, filtObj, level="PSM")
-evaluate_filter(msnid, filtObj, level="Peptide")
-evaluate_filter(msnid, filtObj, level="Accession")
+evaluate_filter(msnid, filtObj, level="peptide")
+evaluate_filter(msnid, filtObj, level="accession")
 # Brute-force optimization by enumeration the criteria combinations.
 # The results should be good starting parameters for follow-up 
 # fine tuning optimizations.
 system.time({
-   filtObj.grid <- optimize_filter(filtObj, msnid, fdr.max=0.01, 
-                                   method="Grid", level="Peptide", n.iter=500)})
+    filtObj.grid <- optimize_filter(filtObj, msnid, fdr.max=0.01, 
+                                    method="Grid", level="peptide", 
+                                    n.iter=500)})
 show(filtObj.grid)
 # Fine tuning. Nelder-Mead optimization.
 system.time({
-   filtObj.nm <- optimize_filter(filtObj.grid, msnid, fdr.max=0.01, 
-                                 method="Nelder-Mead", level="Peptide", n.iter=500)})
+    filtObj.nm <- optimize_filter(filtObj.grid, msnid, fdr.max=0.01, 
+                                    method="Nelder-Mead", level="peptide",
+                                    n.iter=500)})
 show(filtObj.nm)
 # visualize filter
 ggplot(data=params, aes(x=msmsScore, y=absParentMassErrorPPM, color=isDecoy)) +
-   geom_point(size=1.5) + 
-   geom_hline(yintercept=filtObj.nm$absParentMassErrorPPM$threshold, 
-              linetype='dashed') + 
-   geom_vline(xintercept=filtObj.nm$msmsScore$threshold, 
-              linetype='dashed')
+    geom_point(size=1.5) + 
+    geom_hline(yintercept=filtObj.nm$absParentMassErrorPPM$threshold, 
+                linetype='dashed') + 
+    geom_vline(xintercept=filtObj.nm$msmsScore$threshold, 
+                linetype='dashed')
 # compare original and optimized filters
-evaluate_filter(msnid, filtObj, level="Peptide")
-evaluate_filter(msnid, filtObj.nm, level="Peptide")
+evaluate_filter(msnid, filtObj, level="peptide")
+evaluate_filter(msnid, filtObj.nm, level="peptide")
 # filtering the MSnID object
 msnid <- apply_filter(msnid, filtObj.nm)
 show(msnid)
--- inst/unitTests/test_apply_filter.R
+++ inst/unitTests/test_apply_filter.R
@@ -2,36 +2,36 @@ library("MSnID")
 data(c_elegans)
 
 test_apply_filter_1 <- function(){
-   # checking string filter
-   out <- apply_filter(msnidObj, "`ms-gf:specevalue` < 10^-10")
-   checkEquals(id_quality(out, level="PSM"),
-               list(fdr=0.000285103349964362, n=7017))
-   checkEquals(id_quality(out, level="Peptide"),
-               list(fdr=0.000778816199376947, n=2570))
-   checkEquals(id_quality(out, level="Accession"),
-               list(fdr=0.00193986420950533, n=1033))
+    # checking string filter
+    out <- apply_filter(msnidObj, "`MS-GF:SpecEValue` < 10^-10")
+    checkEquals(id_quality(out, level="PSM"),
+                list(fdr=0.000285103349964362, n=7017))
+    checkEquals(id_quality(out, level="peptide"),
+                list(fdr=0.000778816199376947, n=2570))
+    checkEquals(id_quality(out, level="accession"),
+                list(fdr=0.00193986420950533, n=1033))
 }
 
 
 test_apply_filter_2 <- function(){
-   # checking object-based filter
-   
-   msnidObj$msmsScore <- -log10(msnidObj$`ms-gf:specevalue`)
-   msnidObj$mzError <- abs(msnidObj$experimentalMassToCharge - 
-                              msnidObj$calculatedMassToCharge)
-   # setting up filter object
-   filtObj <- MSnIDFilter(msnidObj)
-   filtObj$msmsScore <- list(comparison=">", threshold=9.0)
-   filtObj$mzError <- list(comparison="<", threshold=0.2)
-   
-   out <- apply_filter(msnidObj, filtObj)
-   
-   checkEquals(id_quality(out, level="PSM"),
-               list(fdr=0.0011079650375477, n=8132))
-   checkEquals(id_quality(out, level="Peptide"),
-               list(fdr=0.0013550135501355, n=2956))
-   checkEquals(id_quality(out, level="Accession"),
-               list(fdr=0.00801424755120214, n=1132))
+    # checking object-based filter
+
+    msnidObj$msmsScore <- -log10(msnidObj$`MS-GF:SpecEValue`)
+    msnidObj$mzError <- abs(msnidObj$experimentalMassToCharge - 
+                            msnidObj$calculatedMassToCharge)
+    # setting up filter object
+    filtObj <- MSnIDFilter(msnidObj)
+    filtObj$msmsScore <- list(comparison=">", threshold=9.0)
+    filtObj$mzError <- list(comparison="<", threshold=0.2)
+
+    out <- apply_filter(msnidObj, filtObj)
+
+    checkEquals(id_quality(out, level="PSM"),
+                list(fdr=0.0011079650375477, n=8132))
+    checkEquals(id_quality(out, level="peptide"),
+                list(fdr=0.0013550135501355, n=2956))
+    checkEquals(id_quality(out, level="accession"),
+                list(fdr=0.00801424755120214, n=1132))
 }
 
 
