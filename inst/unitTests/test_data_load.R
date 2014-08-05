1911b5e0bdcdefa02b56a87c1a763f7c6417fe1b
Vlad Petyuk <petyuk@gmail.com>
2014-08-04 17:20:46 -0700

    no need to update PSM column names, since it is handled by mzID package now

--- inst/unitTests/test_data_load.R
+++ inst/unitTests/test_data_load.R
@@ -2,23 +2,6 @@ library("MSnID")
 msnid2 <- MSnID(".")
 mzids <- system.file("extdata","c_elegans.mzid.gz",package="MSnID")
 msnid2 <- read_mzIDs(msnid2, mzids)
-# msnid2$Accession <- msnid2$accession
-# msnid2$accession <- NULL
-# msnid2$isDecoy <- msnid2$isdecoy
-# msnid2$isdecoy <- NULL
-# msnid2$calculatedMassToCharge <- msnid2$calculatedmasstocharge
-# msnid2$calculatedmasstocharge <- NULL
-# msnid2$experimentalMassToCharge <- msnid2$experimentalmasstocharge
-# msnid2$experimentalmasstocharge <- NULL
-# msnid2$chargeState <- msnid2$chargestate
-# msnid2$chargestate <- NULL
-# msnid2$spectrumID <- msnid2$spectrumid
-# msnid2$spectrumid <- NULL
-# msnid2$Peptide <- paste(msnid2$Pre, msnid2$PepSeq, msnid2$Post, sep='.')
-# msnid2$pre <- NULL
-# msnid2$post <- NULL
-# msnid2$pepseq <- NULL
-# clean up the cache directory
 unlink(".Rcache", recursive=TRUE)
 
 
@@ -27,7 +10,7 @@ unlink(".Rcache", recursive=TRUE)
 # So I'll compare psms as data.frame only.
 
 test_data_load <- function() {
-   # now, check if it is what it supposed to be
-   data(c_elegans)
-   checkIdentical(psms(msnidObj), psms(msnid2))
+    # now, check if it is what it supposed to be
+    data(c_elegans)
+    checkIdentical(psms(msnidObj), psms(msnid2))
 }
