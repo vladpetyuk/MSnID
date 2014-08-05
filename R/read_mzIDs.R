effd39815a40a2fd490c6ba12c581ee6296a0d4b
Vlad Petyuk <petyuk@gmail.com>
2014-08-04 17:23:48 -0700

    No need to update column names. It is handled by mzID now.

--- R/read_mzIDs.R
+++ R/read_mzIDs.R
@@ -1,71 +1,34 @@
 
-.read_mzIDs.memoized <- function(mzids) {
-   # Try to load cached data, if exists
-   key <- list(mzids)
-   data <- loadCache(key)
-   if (!is.null(data)) {
-      cat("Loaded cached data\n")
-   }else{   
-      cat("Reading from mzIdentMLs ...\n")
-      data <- data.table(flatten(mzID(mzids)))
-      saveCache(data, key=key)
-   }
-   
-   # the piece below will be removed in the future,
-   # one the corresponding functionality implemented in mzID
-   # do some cleaning/tweaking of column modes and name casing
-   
-   # Columns that must be present:
-   # Peptide, Accession, isDecoy, calculatedMassToCharge, 
-   # experimentalMassToCharge, chargeState, spectrumFile, spectrumID
-   if(!is.null(data$accession)){
-      data$Accession <- data$accession
-      data$accession <- NULL
-   }
+.read_mzIDs.memoized <- function(mzids)
+{
+    # Try to load cached data, if exists
+    key <- list(mzids)
+    data <- loadCache(key)
+    if (!is.null(data)){
+        cat("Loaded cached data\n")
+    }else{   
+        cat("Reading from mzIdentMLs ...\n")
+        data <- data.table(flatten(mzID(mzids), safeNames=FALSE))
+        saveCache(data, key=key)
+    }
 
-   if(!is.null(data$isdecoy)){
-      data$isDecoy <- data$isdecoy
-      data$isdecoy <- NULL
-   }
-   
-   if(!is.null(data$calculatedmasstocharge)){
-      data$calculatedMassToCharge <- as.numeric(data$calculatedmasstocharge)
-      data$calculatedmasstocharge <- NULL
-   }
-   
-   if(!is.null(data$experimentalmasstocharge)){
-      data$experimentalMassToCharge <- as.numeric(data$experimentalmasstocharge)
-      data$experimentalmasstocharge <- NULL
-   }
-   
-   if(!is.null(data$chargestate)){
-      data$chargeState <- data$chargestate
-      data$chargestate <- NULL
-   }
-   
-   if(!is.null(data$spectrumid)){
-      data$spectrumID <- data$spectrumid
-      data$spectrumid <- NULL
-   }
-   
-   if(!is.null(data$pre)){
-      data$Pre <- data$pre
-      data$pre <- NULL
-   }
-   
-   if(!is.null(data$post)){
-      data$Post <- data$post
-      data$post <- NULL
-   }
-   
-   if(!is.null(data$pepseq)){
-      data$PepSeq <- data$pepseq
-      data$pepseq <- NULL
-   }
-   #---
-   data$Peptide <- paste(data$Pre, data$PepSeq, data$Post, sep='.')
-   
-   return(data)
+    # Adding peptide identification in a conventional format:
+    # X.XXXXX.X, where pre and post amino acids separated by dots 
+    # from the main sequence
+    # This may be substituted in the future with more flexible
+    # peptide S4 object
+    data$peptide <- paste(data$pre, data$pepSeq, data$post, sep='.')
+
+    # Columns that must be present:
+    # peptide, accession, isDecoy, calculatedMassToCharge, 
+    # experimentalMassToCharge, chargeState, spectrumFile, spectrumID
+    # There is no checking for these columns at this point.
+    # If the mzIdentML file conformed with PSI standard, most likely
+    # those columns are present.
+    # There are checks for columns in some of the other functions
+    # that rely on presence of particular columns.
+    
+    return(data)
 }
 
 
