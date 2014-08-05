9c5e92d209de86cf50a76fe10c19fc706d0c941d
Vlad Petyuk <petyuk@gmail.com>
2014-08-04 17:10:20 -0700

    changing tabs

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -3,20 +3,18 @@
 .PROTON_MASS <- 1.007276466812
 
 setClass(Class="MSnID",
-         representation(
-            workDir="character", # working directory
-            psms="data.table" # peptide-to-spectra matches
-            )
-         )
+            representation(
+                workDir="character", # working directory
+                psms="data.table") # peptide-to-spectra matches
+)
 
 
 
 
 # partitions and parameter transforms to be added to filter class
 setClass(Class="MSnIDFilter",
-         representation(
-            filterList="list",
-            validParNames="character"
-         )
+            representation(
+                filterList="list",
+                validParNames="character")
 )
 
