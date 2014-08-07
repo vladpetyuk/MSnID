542a7171c45eb674a426fbe41736415fa8f6b67f
Vlad Petyuk <petyuk@gmail.com>
2014-08-06 22:37:53 -0700

    switch from list to named vector

--- inst/unitTests/test_apply_filter.R
+++ inst/unitTests/test_apply_filter.R
@@ -5,11 +5,11 @@ test_apply_filter_1 <- function(){
     # checking string filter
     out <- apply_filter(msnidObj, "`MS-GF:SpecEValue` < 10^-10")
     checkEquals(id_quality(out, level="PSM"),
-                list(fdr=0.000285103349964362, n=7017))
+                c(fdr=0.000285103349964362, n=7017))
     checkEquals(id_quality(out, level="peptide"),
-                list(fdr=0.000778816199376947, n=2570))
+                c(fdr=0.000778816199376947, n=2570))
     checkEquals(id_quality(out, level="accession"),
-                list(fdr=0.00193986420950533, n=1033))
+                c(fdr=0.00193986420950533, n=1033))
 }
 
 
@@ -27,11 +27,11 @@ test_apply_filter_2 <- function(){
     out <- apply_filter(msnidObj, filtObj)
 
     checkEquals(id_quality(out, level="PSM"),
-                list(fdr=0.0011079650375477, n=8132))
+                c(fdr=0.0011079650375477, n=8132))
     checkEquals(id_quality(out, level="peptide"),
-                list(fdr=0.0013550135501355, n=2956))
+                c(fdr=0.0013550135501355, n=2956))
     checkEquals(id_quality(out, level="accession"),
-                list(fdr=0.00801424755120214, n=1132))
+                c(fdr=0.00801424755120214, n=1132))
 }
 
 
