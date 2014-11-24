66cea703970daa3b69afa62dd8a22b454c9bf4d0
v.petyuk <v.petyuk@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-11-24 05:09:04 +0000

    setting up git-svn bridge
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MSnID@97059 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/optimize_filter.r
+++ R/optimize_filter.r
@@ -1,17 +1,30 @@
 # methods and functions for handling filter optimization.
 
-.get_num_pep_for_fdr <- function(filterThresholds, 
-                                    msmsdata, 
-                                    filter, 
-                                    fdr.max, 
-                                    ...) 
+
+.msg.invalid.optimization.results <-
+    paste("No settings that satisfy FDR criteria.",
+          "Returning original filter.",
+          sep='\n')
+
+# later warning messages for different optimization
+# issues will be customized
+.msg.invalid.optimization.results.grid <-
+    .msg.invalid.optimization.results
+
+
+
+.get_num_pep_for_fdr <- function(filterThresholds,
+                                    msmsdata,
+                                    filter,
+                                    fdr.max,
+                                    ...)
 {
     filter <- update(filter, filterThresholds)
     # level should get here through ... (ellipsis)
     x <- evaluate_filter(msmsdata, filter, ...)
     if(is.nan(x[['fdr']]) || x[['fdr']] > fdr.max){
         # 0 is bad because optimization does not move
-        return(rnorm(1,sd=0.001)) 
+        return(rnorm(1,sd=0.001))
     }else{
         return(x[['n']])
     }
@@ -20,11 +33,11 @@
 
 .construct_optimization_grid <- function(filterObj, msnidObj, n.iter)
 {
-    n.iter.per.param <- round(n.iter^(1/length(filterObj))) - 1 
+    n.iter.per.param <- round(n.iter^(1/length(filterObj))) - 1
     probs <- seq(0, 1, 1/n.iter.per.param)
-    eg <- expand.grid(lapply(names(filterObj), 
-                                function(arg) 
-                                    quantile(msnidObj[[arg]], 
+    eg <- expand.grid(lapply(names(filterObj),
+                                function(arg)
+                                    quantile(msnidObj[[arg]],
                                                 probs, na.rm=TRUE)))
     colnames(eg) <- names(filterObj)
     return(eg)
@@ -34,32 +47,41 @@
 .optimize_filter.grid <- function(filterObj, msnidObj, fdr.max, level, n.iter)
 {
     par.grid <- .construct_optimization_grid(filterObj, msnidObj, n.iter)
-    evaluations <- apply(X=par.grid, 
-                            MARGIN=1, 
-                            FUN=.get_num_pep_for_fdr, 
-                                msnidObj, 
-                                filterObj, 
-                                fdr.max, 
+    evaluations <- apply(X=par.grid,
+                            MARGIN=1,
+                            FUN=.get_num_pep_for_fdr,
+                                msnidObj,
+                                filterObj,
+                                fdr.max,
                                 level)
+    evaluations <- round(evaluations)
+    if(all(evaluations == 0)){
+        warning(.msg.invalid.optimization.results.grid)
+        return(filterObj)
+    }
     optim.pars <- par.grid[which.max(evaluations),]
     newFilter <- update(filterObj, as.numeric(optim.pars))
     return(newFilter)
 }
 
 
-.optimize_filter.grid.mclapply <- function(filterObj, msnidObj, 
+.optimize_filter.grid.mclapply <- function(filterObj, msnidObj,
                                             fdr.max, level, n.iter)
 {
     par.grid <- .construct_optimization_grid(filterObj, msnidObj, n.iter)
-    evaluations <- mclapply(seq_len(nrow(par.grid)), 
+    evaluations <- mclapply(seq_len(nrow(par.grid)),
                             function(i){
-                                .get_num_pep_for_fdr(par.grid[i,], 
-                                                        msnidObj, 
-                                                        filterObj, 
-                                                        fdr.max, 
-                                                        level)}, 
+                                .get_num_pep_for_fdr(par.grid[i,],
+                                                        msnidObj,
+                                                        filterObj,
+                                                        fdr.max,
+                                                        level)},
                             mc.cores=detectCores())
-    evaluations <- unlist(evaluations)
+    evaluations <- round(unlist(evaluations))
+    if(all(evaluations == 0)){
+        warning(.msg.invalid.optimization.results.grid)
+        return(filterObj)
+    }
     optim.pars <- par.grid[which.max(evaluations),]
     newFilter <- update(filterObj, as.numeric(optim.pars))
     return(newFilter)
@@ -68,17 +90,17 @@
 
 setMethod("optimize_filter",
             signature(filterObj="MSnIDFilter", msnidObj="MSnID"),
-            definition=function(filterObj, msnidObj, fdr.max, 
+            definition=function(filterObj, msnidObj, fdr.max,
                                    method, level, n.iter)
             {
-                .optimize_filter.memoized(filterObj, msnidObj, 
+                .optimize_filter.memoized(filterObj, msnidObj,
                                             fdr.max, method, level, n.iter)
             }
 )
 
 
 
-.optimize_filter <- function(filterObj, msnidObj, 
+.optimize_filter <- function(filterObj, msnidObj,
                                fdr.max, method, level, n.iter)
 {
     method <- match.arg(method, choices=c("Grid", "Nelder-Mead", "SANN"))
@@ -86,24 +108,24 @@ setMethod("optimize_filter",
     #
     # subset msnidObj to only relevant columns
     if(level == "PSM"){
-       msnidObj@psms <- 
+       msnidObj@psms <-
            msnidObj@psms[,c("isDecoy", names(filterObj)), with=FALSE]
     }else{
-       msnidObj@psms <- 
+       msnidObj@psms <-
            msnidObj@psms[,c("isDecoy", level, names(filterObj)), with=FALSE]
+       # substitute Peptide and or Accession with integers
+       msnidObj@psms[[level]] <- as.integer(as.factor(msnidObj@psms[[level]]))
     }
-    # substitute Peptide and or Accession with integers
-    msnidObj@psms[[level]] <- as.integer(as.factor(msnidObj@psms[[level]]))
     #
     if(method == "Grid"){
         if(.Platform$OS.type == "unix"){
-            optimFilter <- 
-                .optimize_filter.grid.mclapply(filterObj, msnidObj, 
+            optimFilter <-
+                .optimize_filter.grid.mclapply(filterObj, msnidObj,
                                                 fdr.max, level, n.iter)
         }else{
             # yet to implement effective parallelization on Windows
-            optimFilter <- 
-                .optimize_filter.grid(filterObj, msnidObj, 
+            optimFilter <-
+                .optimize_filter.grid(filterObj, msnidObj,
                                         fdr.max, level, n.iter)
         }
     }
@@ -117,6 +139,11 @@ setMethod("optimize_filter",
                             method = method,
                             control=list(fnscale=-1, maxit=n.iter))
         optimFilter <- update(filterObj, as.numeric(optim.out$par))
+        x <- evaluate_filter(msmsdata, optimFilter, level)
+        if(is.nan(x[['fdr']]) || x[['fdr']] > fdr.max){
+            warning(.msg.invalid.optimization.results)
+            return(filterObj)
+        }
     }
     return(optimFilter)
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+MSnID
+====
+`MSnID` is an R/Bioconductor package that provides convience tools for handling and filtering of MS/MS identification results. The official page is the Bioconductor landing page ([release](http://www.bioconductor.org/packages/release/bioc/html/MSnID.html) or [devel](http://www.bioconductor.org/packages/devel/bioc/html/MSnID.html) versions). The [github page](https://github.com/vladpetyuk/MSnID) page is for sharing, testing, issue tracking and forking/pulling purposes.
