38be65e949cc84cbd6c85b87727817029848cd21
v.petyuk <v.petyuk@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-06-16 06:19:01 +0000

    unit test for PSM FDR. commit to devel
    From: Vlad Petyuk <petyuk@gmail.com>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MSnID@118642 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_apply_filter.R
+++ inst/unitTests/test_apply_filter.R
@@ -5,7 +5,20 @@ test_apply_filter_1 <- function(){
     # testing string filter
     out <- apply_filter(msnidObj, "`MS-GF:SpecEValue` < 10^-10")
     # cheching against expected values
-    ref <- matrix(c(0.000285103349964362, 7017,
+    
+    # these are old values that ignored peptide-to-protein
+    # matching redundancy.
+    # Left them here just in case.
+    # ref <- matrix(c(0.000285103349964362, 7017,
+    #                 0.000778816199376947, 2570,
+    #                 0.00193986420950533 , 1033),
+    #               ncol=2, nrow=3, byrow=TRUE,
+    #               dimnames=list(c("PSM","peptide","accession"),
+    #                             c("fdr","n")))
+    
+    # new PSM FDR values that relies on calculation that 
+    # removes protein assignments first
+    ref <- matrix(c(0.0005005005005005, 3998,
                     0.000778816199376947, 2570,
                     0.00193986420950533 , 1033),
                   ncol=2, nrow=3, byrow=TRUE,
@@ -27,13 +40,25 @@ test_apply_filter_2 <- function(){
     filtObj$mzError <- list(comparison="<", threshold=0.2)
     # appying filter
     out <- apply_filter(msnidObj, filtObj)
+    
     # cheching against expected values
-    ref <- matrix(c(0.0011079650375477 , 8132,
-                    0.0013550135501355 , 2956,
+    
+    # old values (see above)
+    # ref <- matrix(c(0.0011079650375477 , 8132,
+    #                 0.0013550135501355 , 2956,
+    #                 0.00801424755120214, 1132),
+    #               ncol=2, nrow=3, byrow=TRUE,
+    #               dimnames=list(c("PSM","peptide","accession"),
+    #                             c("fdr","n")))
+     
+    # new values (see above)
+    ref <- matrix(c(0.00087260034904014, 4588,
+                    0.0013550135501355, 2956,
                     0.00801424755120214, 1132),
                   ncol=2, nrow=3, byrow=TRUE,
                   dimnames=list(c("PSM","peptide","accession"),
                                 c("fdr","n")))
+    
     checkEqualsNumeric(id_quality(out), ref)
 }
 
